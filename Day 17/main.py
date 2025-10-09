579dd9c8c3ba1d5c692279651b4b73808bd9127c
cassvahack <oise@elios.co>
2025-10-08 23:59:47 -0400

    100 days of code

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,19 +1,47 @@
+# class question:
+#
+#     def __init__(self, text, answer):
+#
+#     def follow(self,user):
+#         user.followers += 1
+#         self.following += 1
+#
+# user_1 = User("001","joe")
+#
+# user_2 = User("002","bob")
+#
+#
+#
+# user_1.follow(user_2)
+#
+#
+# print(user_1.followers)
+
+
+
+
+
 class User:
 
-    def __init__(self,user_id, username):
+    def __init__(self, user_id,username):
         self.id = user_id
         self.username = username
         self.followers = 0
         self.following = 0
 
-
     def follow(self,user):
         user.followers += 1
         self.following += 1
 
-user_1 = User("001","joe")
-
-user_2 = User("002","bob")
 
 
+user_1 = User("001","angela")
+
+
+
+user_2 = User("002", "jack")
+
+user_1.follow(user_2)
+
+print(user_1.followers)
 print(user_1.followers)
